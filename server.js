21cd19c9d7838f23fc704568f97d0e56fb80e522
toluwanimisamuel741-png <toluwanimisamuel741@gmail.com>
2026-07-01 15:29:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("🤖 AyoXpert AI Bot is Running!");
-});
+const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`AyoXpert started on port ${PORT}`);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  res.end("🚀 AyoXpert Telegram AI Bot is running!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
