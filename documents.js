54b22d545e80b3c3e7044cf339c181ba89b517f4
toluwanimisamuel741-png <toluwanimisamuel741@gmail.com>
2026-07-03 14:28:11 -0700

    Update documents.js

--- documents.js
+++ documents.js
@@ -1,1 +1,24 @@
+// Stores the latest document for each user
 
+const documents = {};
+
+// Save document text
+function saveDocument(chatId, text) {
+  documents[chatId] = text;
+}
+
+// Get document text
+function getDocument(chatId) {
+  return documents[chatId] || null;
+}
+
+// Remove document
+function clearDocument(chatId) {
+  delete documents[chatId];
+}
+
+module.exports = {
+  saveDocument,
+  getDocument,
+  clearDocument
+};
